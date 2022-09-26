c2fba4d92facf69cafd6344518db60dda867e557
AntoniNeutron <antonineutron@gmail.com>
2022-09-26 21:11:06 +0100

    September 26 class, Product model creted, Glide library integrated, and search menu added

--- app/src/main/java/com/tech4dev/wmtstore/data/models/Product.kt
+++ app/src/main/java/com/tech4dev/wmtstore/data/models/Product.kt
@@ -1,3 +1,10 @@
 package com.tech4dev.wmtstore.data.models
 
-data class product()
+data class Product(
+    val id: String,
+    val image: String,
+    val name: String,
+    val price: Double,
+    val seller: String,
+    val size: String
+)
--- app/src/main/java/com/tech4dev/wmtstore/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tech4dev/wmtstore/ui/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.tech4dev.wmtstore.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.tech4dev.wmtstore.R
 import com.tech4dev.wmtstore.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +15,11 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,5 +42,9 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
     }
 }
